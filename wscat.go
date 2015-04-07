cc55a0a3ebd0c1e4179b7df4728a1c502c11248a
XiaokangWang <xiaokangwang@outlook.com>
2015-04-07 18:59:56 +0800

    Check arg

---
+++ wscat.go
@@ -0,0 +1,22 @@
+package main
+
+import "os"
+import "fmt"
+import "golang.org/x/net/websocket"
+import "log"
+
+func main() {
+	if os.Args[0] == "" || len(os.Args) < 2 || len(os.Args) >= 3 {
+		log.Fatal("invalid argument")
+		os.Exit(-1)
+	}
+
+	origin := "https://kkdev.org/?wscatinit"
+	url := os.Args[1]
+
+	if len(os.Args) == 2 {
+		origin = ""
+	}
+
+
+}
