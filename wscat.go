a543aafba9708d3f4acaaba2f40d9115dccdada4
XiaokangWang <xiaokangwang@outlook.com>
2015-04-08 18:55:18 +0800

    No def origin

--- wscat.go
+++ wscat.go
@@ -4,6 +4,8 @@ import "os"
 import "fmt"
 import "golang.org/x/net/websocket"
 import "log"
+import "net/url"
+import "strings"
 
 func ws_in(ws *websocket.Conn) {
 
@@ -34,37 +36,10 @@ func ws_in(ws *websocket.Conn) {
 
 }
 
-func main() {
-
-	if os.Args[0] == "" || len(os.Args) < 2 || len(os.Args) > 3 {
-		log.Fatal("invalid argument")
-		os.Exit(-1)
-	}
-
-	origin := "https://kkdev.org/?wscatinit"
-	urli := os.Args[1]
-
-	if len(os.Args) == 2 {
-		//origin = ""
-	} else {
-		origin = os.Args[2]
-	}
-
-	log.Println("Connecting to " + urli)
-
-	ws, err := websocket.Dial(urli, "", origin)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	go ws_in(ws)
-
-	log.Println("Ready")
-
+func ws_out(ws *websocket.Conn) {
 	for true {
 		var so string
-		_, err = fmt.Scanf("%s\n", &so)
+		_, err := fmt.Scanf("%s\n", &so)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -76,6 +51,57 @@ func main() {
 		}
 
 		//log.Println("Sent:" + so)
+	}
+}
+
+func ws_connd(ws *websocket.Conn) {
+	go ws_in(ws)
+
+	log.Println("Ready")
+
+	ws_out(ws)
+}
+
+func main() {
+
+	if os.Args[0] == "" || len(os.Args) < 2 || len(os.Args) > 3 {
+		log.Fatal("invalid argument")
+		os.Exit(-1)
+	}
+
+	origin := ""
+	urli := os.Args[1]
+
+	if len(os.Args) == 2 {
+		//origin = ""
+	} else {
+		origin = os.Args[2]
+	}
+
+	log.Println("Connecting to " + urli)
+
+	if origin == "" {
+		oriu,_:=url.Parse(urli)
+		 switch strings.ToUpper(oriu.Scheme){
+			case "WSS":
+			oriu.Scheme="https"
+			case "WS":
+			oriu.Scheme="http"
+		}
+		origin=oriu.String()
+		ws, err := websocket.Dial(urli, "", origin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ws_connd(ws)
+
+	} else {
+		ws, err := websocket.Dial(urli, "", origin)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ws_connd(ws)
+
 	}
 
 }
